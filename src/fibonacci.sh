916c4b83930090cadc72c10682b5f0206ad69ee2
Mauricio Ballesteros <mballes95@gmail.com>
2017-04-25 03:54:39 -0400

    Fixing Fibonacci

--- src/fibonacci.sh
+++ src/fibonacci.sh
@@ -5,28 +5,26 @@ echo "Welcome to my Fibonacci Script"
 echo -n "Type your number: "
 read NUMBER
 
-if [[ "$NUMBER" -lt 0 || "$NUMBER" -eq 0 ]]; then
+if [[ "$NUMBER" -le 0 ]]; then
 	echo "Your number has to be greater than 0"
 	exit 0
 fi
 
+echo 0
+
 if [[ "$NUMBER" -eq 1 ]]; then
-	echo 0
 	exit 0
 fi
 
-if [[ "$NUMBER" -eq 2 ]]; then
-	echo 0
-	echo 1
+echo 1
+
+if [[ "$NUMBER" -eq 2 ]]; then	
 	exit 0
 fi
 
 BACK=0
 ACTUAL=1
 AUX=0
-
-echo "$BACK"
-echo "$ACTUAL"
 
 while [[ "$NUMBER" -gt 2 ]]; do
 	AUX=$ACTUAL

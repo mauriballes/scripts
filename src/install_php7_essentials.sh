17ca0c267350b285be7a3f850cfd8a012d4501b9
Mauricio Ballesteros <mballes95@gmail.com>
2017-06-24 12:15:31 -0400

    Fixing PHP PPA

--- src/install_php7_essentials.sh
+++ src/install_php7_essentials.sh
@@ -3,6 +3,7 @@
 
 ROOT_UID=0     # Only users with $UID 0 have root privileges.
 E_NOTROOT=87   # Non-root exit error.
+REPO=ondrej/php # PPA PHP
 # Variables are better than hard-coded values.
 
 # Run as root, of course.
@@ -12,33 +13,34 @@ then
   exit $E_NOTROOT
 fi
 
-echo "This script only works with Ubuntu 16.04 or above"
-echo -n "Do you want to continue? [Y/n] "
-
-read ANSWER
-
-if [[ "$ANSWER" == "y" || "$ANSWER" == "Y" ]]
+echo "Installing Packages"
+apt-get install software-properties-common -y
+# Add if you dont have it!
+if ! grep -l "$REPO" /etc/apt/sources.list /etc/apt/sources.list.d/*
 then
-  apt-get install php7.0 \
-  php7.0-cli \
-  php7.0-common \
-  php7.0-curl \
-  php7.0-dev \
-  php7.0-fpm \
-  php7.0-gd \
-  php7.0-json \
-  php7.0-mbstring \
-  php7.0-mcrypt \
-  php7.0-mysql \
-  php7.0-pgsql \
-  php7.0-sqlite3 \
-  php7.0-opcache \
-  php7.0-readline \
-  php7.0-xml \
-  php7.0-zip -y
-  echo "#################"
-  echo "Packages Installed!"
-  echo "#################"
-  exit 0
+  add-apt-repository ppa:$REPO -y
 fi
-exit 1
+
+echo "Installing PHP7"
+apt-get install php7.0 \
+php7.0-cli \
+php7.0-common \
+php7.0-curl \
+php7.0-dev \
+php7.0-fpm \
+php7.0-gd \
+php7.0-json \
+php7.0-mbstring \
+php7.0-mcrypt \
+php7.0-mysql \
+php7.0-pgsql \
+php7.0-sqlite3 \
+php7.0-opcache \
+php7.0-readline \
+php7.0-xml \
+php7.0-zip -y
+echo "#################"
+echo "Packages Installed!"
+echo "#################"
+
+exit 0
